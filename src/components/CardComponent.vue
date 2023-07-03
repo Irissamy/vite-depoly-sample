313e1e403dbfd8c1542e3c459d818d431046e063
iris_lai <amy40522a@gmail.com>
2023-07-03 15:16:23 +0800

    first commit

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>This is an Card</h1>
+        <h3>{{ text }}</h3>
+    </div>
+</template>
+
+<script>
+
+export default{
+    data() {
+        return {
+            text: 'this is card data'
+        }
+    }
+}
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AboutView',
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    const api = import.meta.env.VITE_PATH
+    this.$http.get(api)
+      .then((res) => {
+        console.log(res.data)
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
